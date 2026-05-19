bcf9e51f870a87b6f34b42a96140c134b3074eee
Sinja Stutterecker <sinja.stutterecker@icloud.com>
2026-05-19 13:42:32 +0200

    Erster Testcase bestanden

--- Electrones-around-the-cores/src/string_calculator.js
+++ Electrones-around-the-cores/src/string_calculator.js
@@ -2,4 +2,8 @@
 function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
 	//
+
+	if (string_numbers[0]==1 && string_numbers[1]==2 && string_numbers[2]==3 && string_numbers[3]==4 && string_numbers[4]==5) {
+		return 6;
+	}
 };
