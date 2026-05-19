a7370dcc3cf51452e9683bffbca6043f50d2a02a
Max <cmgyfx9wmw@privaterelay.appleid.com>
2026-05-19 14:10:52 +0200

    Dritter Testcase [ 6, 6, 4, 4, 1, 3 ] sollte 2 sein.

--- Electrones-around-the-cores/spec/string_calculator_spec.js
+++ Electrones-around-the-cores/spec/string_calculator_spec.js
@@ -10,4 +10,8 @@ describe('String Calculator', function() {
 	it('should return 4 for array [ 2, 2, 3, 3 ] ', function(){
 		expect(calculator.add([ 2, 2, 3, 3 ])).toEqual(4);
 	});
+
+	it('should return 2 for array [ 6, 6, 4, 4, 1, 3 ] ', function() {
+		expect(calculator.add([ 6, 6, 4, 4, 1, 3 ])).toEqual(2);
+	});
 });
