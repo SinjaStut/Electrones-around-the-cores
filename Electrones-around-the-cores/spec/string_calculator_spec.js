7e72989f21be659dd9051151a7af85c529ecaf65
Max <cmgyfx9wmw@privaterelay.appleid.com>
2026-05-19 13:31:22 +0200

    Erster Testcase [ 1, 2, 3, 4, 5 ] sollte 6 sein.

--- Electrones-around-the-cores/spec/string_calculator_spec.js
+++ Electrones-around-the-cores/spec/string_calculator_spec.js
@@ -3,7 +3,7 @@ describe('String Calculator', function() {
 		calculator = new StringCalculator();
 	});
 
-	it('should return 0 for an empty string', function() {
-		expect(calculator.add('')).toEqual(0);
+	it('should return 6 for array [ 1, 2, 3, 4, 5 ]', function() {
+		expect(calculator.add([ 1, 2, 3, 4, 5 ])).toEqual(6);
 	});
 });
