274f05ff31f2f412dd3452509ba6f676b3bb86c0
Max <cmgyfx9wmw@privaterelay.appleid.com>
2026-05-19 14:22:30 +0200

    4. Testcase bestanden

--- Electrones-around-the-cores/src/string_calculator.js
+++ Electrones-around-the-cores/src/string_calculator.js
@@ -2,6 +2,13 @@
 function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
 	//
+	return string_numbers.reduce((akkumulator, x) => {
+		if (x % 2 !== 0) {
+			return akkumulator + x - 1;
+		} else {
+			return akkumulator;
+		}
+	}, 0);
 
 	if (string_numbers[0]==1 && string_numbers[1]==2 && string_numbers[2]==3 && string_numbers[3]==4 && string_numbers[4]==5) {
 		return 6;
